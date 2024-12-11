082c883aa10168706766ec6e80ec522277a4d6bf
Mohand AIT AMARA <aitamarar@gmail.com>
2024-12-11 19:40:58 +0100

    FIX nom de classes et relationship

--- src/apps/schedules/model/years_groups_educational_courses/years_groups_educational_courses_model.py
+++ src/apps/schedules/model/years_groups_educational_courses/years_groups_educational_courses_model.py
@@ -9,6 +9,6 @@ class YearsGroupsEducationalCourses(Base):
     educational_courses_id: Mapped[int] = mapped_column(ForeignKey('educational_courses.id'), primary_key=True)
     day_type_id: Mapped[int] = mapped_column(ForeignKey('day_type.id'), nullable=False)
 
-    years_group = relationship("YearsGroup", backref="years_groups_educational_courses")
-    educational_course = relationship("EducationalCourse", backref="years_groups_educational_courses")
+    years_group = relationship("YearsGroups", backref="years_groups_educational_courses")
+    educational_course = relationship("EducationalCourses", backref="years_groups_educational_courses")
     day_type = relationship("DayType", backref="years_groups_educational_courses")
