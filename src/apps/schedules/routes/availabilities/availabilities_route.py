4e105de266f238bee59d01fb5c89bf792b50c832
Omer Dotchamou <omer.dotchamou@scalens.com>
2024-12-15 00:58:51 +0100

    Update route and service related to availabilities for better data and error handling

--- src/apps/schedules/routes/availabilities/availabilities_route.py
+++ src/apps/schedules/routes/availabilities/availabilities_route.py
@@ -1,52 +1,67 @@
 from fastapi import APIRouter, Depends, HTTPException
 from sqlalchemy.ext.asyncio import AsyncSession
+
 from src.config.database_service import get_db
-from src.apps.schedules.services.availabilities.availabilities_service import (
-    get_availabilities,
-    get_availabilities_by_users_id,
-    create_availability,
-    update_availability,
-    delete_availability
-)
+from src.apps.schedules.services.availabilities.availabilities_service import AvailabilityService
 from src.apps.schedules.model.availabilities.availabilities_schema import (
-    AvailabilityCreate,
-    AvailabilityUpdate,
-    AvailabilityResponse
+    AvailabilityCreateSchema,
+    AvailabilityUpdateSchema,
+    AvailabilityResponseSchema
 )
+from src.helpers import SecurityHelper
 
-router = APIRouter(prefix="/availabilities")
+router = APIRouter(prefix="/availabilities", tags=["Availabilities"])
 
-@router.get("/", response_model=list[AvailabilityResponse])
-async def read_availabilities(db: AsyncSession = Depends(get_db)):
-    return await get_availabilities(db)
-
-@router.get("/user/{users_id}", response_model=list[AvailabilityResponse])
-async def read_availability_by_users_id(users_id: int, db: AsyncSession = Depends(get_db)):
-    availability = await get_availabilities_by_users_id(db, users_id)
-    if not availability:
-        raise HTTPException(status_code=404, detail="No availabilities found for this user")
-    return availability
-
-@router.post("/", response_model=AvailabilityResponse)
-async def create_new_availability(availability: AvailabilityCreate, db: AsyncSession = Depends(get_db)):
+@router.get("/", response_model=list[AvailabilityResponseSchema])
+async def read_availability_by_users_id(
+    current_user: dict = Depends(SecurityHelper.get_current_user),
+    session: AsyncSession = Depends(get_db)
+):
     try:
-        return await create_availability(db, availability)
+        user_id = current_user["sub"]
+        availability = await AvailabilityService.get_availabilities_by_users_id(session, user_id)
+        return availability
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
-@router.put("/{availability_id}", response_model=AvailabilityResponse)
-async def update_existing_availability(availability_id: int, availability: AvailabilityUpdate, db: AsyncSession = Depends(get_db)):
+@router.post("/", response_model=AvailabilityResponseSchema)
+async def create_new_availability(
+    availability: AvailabilityCreateSchema,
+    current_user: dict = Depends(SecurityHelper.get_current_user), 
+    session: AsyncSession = Depends(get_db)
+):
     try:
-        updated = await update_availability(db, availability_id, availability)
-        if not updated:
-            raise HTTPException(status_code=404, detail="Availability not found")
-        return updated
+        slots = availability.dict().get('slots')
+        user_id = current_user.get("sub")
+        availability = await AvailabilityService.create_availability(session, slots, user_id)
+        return availability
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+
+@router.put("/{availability_id}", response_model=AvailabilityResponseSchema)
+async def update_existing_availability(
+    availability_id: int, 
+    availability: AvailabilityUpdateSchema,
+    current_user: dict = Depends(SecurityHelper.get_current_user), 
+    session: AsyncSession = Depends(get_db)
+):
+    try:
+        slots = availability.dict().get('slots')
+        user_id = current_user["sub"]
+        updated_availability = await AvailabilityService.update_availability(session, availability_id, slots, user_id)
+        return updated_availability
     except ValueError as e:
         raise HTTPException(status_code=400, detail=str(e))
 
 @router.delete("/{availability_id}")
-async def delete_existing_availability(availability_id: int, db: AsyncSession = Depends(get_db)):
-    deleted = await delete_availability(db, availability_id)
-    if not deleted:
-        raise HTTPException(status_code=404, detail="Availability not found")
-    return {"message": "Availability deleted successfully"}
+async def delete_existing_availability(
+    availability_id: int, 
+    current_user: dict = Depends(SecurityHelper.get_current_user),
+    session: AsyncSession = Depends(get_db)
+):
+    try:
+        await AvailabilityService.delete_availability(session, availability_id)
+        return {"message": "Availability deleted successfully"}
+    except ValueError as e:
+        raise HTTPException(status_code=400, detail=str(e))
+    
